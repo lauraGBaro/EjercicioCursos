59f921d78a9fb1f4ee7811910de5c76725046549
lauraGBaro <lauragayanbaro@gmail.com>
2019-09-09 23:29:59 +0200

    Arreglada cadena de conexión para permitir configurarla desde al app.settings

--- EjercicioCodeFirstCursos/Startup.cs
+++ EjercicioCodeFirstCursos/Startup.cs
@@ -31,9 +31,10 @@ namespace EjercicioCodeFirstCursos
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-			var connection = @"Server=(localdb)\mssqllocaldb;Database=EjercicioCodeFirstCursos;Trusted_Connection=True;ConnectRetryCount=0";
-			services.AddDbContext<CursosContext>
-				(options => options.UseSqlServer(connection));
+			//var connection = @"Server=(localdb)\mssqllocaldb;Database=EjercicioCodeFirstCursos;Trusted_Connection=True;ConnectRetryCount=0";
+
+			var connection = Configuration["ConnectionStrings:DefaultConnection"];
+			services.AddDbContext<CursosContext>(options => options.UseSqlServer(connection));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
